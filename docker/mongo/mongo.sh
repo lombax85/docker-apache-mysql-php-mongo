ddf49f639f0fa82a5049d6723b9f75a542914cbf
Fabio <lombax85@gmail.com>
2016-10-08 15:17:56 +0200

    fix mongo script

--- docker/mongo/mongo.sh
+++ docker/mongo/mongo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mongo admin --eval "db.createUser({ user: '$0', pwd: '$1', roles: [ { role: 'userAdminAnyDatabase', db: 'admin' } ] })"
+mongo admin --eval "db.createUser({ user: '$1', pwd: '$2', roles: [ { role: 'userAdminAnyDatabase', db: 'admin' } ] })"
